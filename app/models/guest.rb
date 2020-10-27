daecb42e612ce5b7333c02de0f27df06e1f981ee
github username <github email address>
2020-10-27 14:17:06 -0700

    seeds data

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,5 +1,5 @@
 class Guest < ApplicationRecord
     belongs_to :house
-    validates :name, :phone_number, :email presence: true
+    validates :name, :phone_number, :email, presence: true
 end
-end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# create_table :guests do |t|
+#     t.string :name
+#     t.string :phone_number
+#     t.string :guest_address
+#     t.string :email
+#     t.string :time_line
+#     t.string :comment
+#     t.references :house, null:false, foreign_key: true
+
+# create_table :houses do |t|
+#     t.string :house_address
+
+house = House.create(house_address: "testhouseadress")
+guest = Guest.create(name: "nametest", phone_number: "phonenumbertest",guest_address: "guestsaddresstest",email: "emailtest",time_line:"timelinetest", comment:"commenttest", house_id:1)
+guest_two = Guest.create(name: "nametest", phone_number: "phonenumbertest",guest_address: "guestsaddresstesttwo",email: "emailtesttwo",time_line:"timelinetesttwo", comment:"commenttesttwo", house_id:2)
