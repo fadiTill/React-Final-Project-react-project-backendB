6a5ef56f2fdbe4197e8c30675cd4c47e7494a101
github username <github email address>
2020-11-02 09:50:08 -0800

    set up

--- react-frontend-c/src/index.js
+++ react-frontend-c/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from 'redux'
 // import './index.css';
+import thunk from 'redux-thunk'
+import {provider} from 'react-redux'
 import App from './App';
 
 
