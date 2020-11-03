ebebd5707d4a4a2f619c146e170ebe48a1f5c50e
github username <github email address>
2020-11-03 09:23:04 -0800

    set up index, create store

--- react-frontend-c/src/index.js
+++ react-frontend-c/src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 // import './index.css';
 import thunk from 'redux-thunk'
 import {provider} from 'react-redux'
 import App from './App';
 
+ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-ReactDOM.render(<App />,document.getElementById('root'));
+
+let reducer = ''
+
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
+ReactDOM.render(
+    <provider store={store}> 
+     <App />
+    </provider>
+
+
+,document.getElementById('root'));
 
